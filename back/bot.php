e51025bd6c05ce04598348f6665c73b1ac6d0eed
Ethan <easonbest123@gmail.com>
2024-01-22 10:54:05 +0800

    後台編輯頁尾版權頁面 完成

---
+++ back/bot.php
@@ -0,0 +1,16 @@
+<h2 class="ct">編輯頁尾版權區</h2>
+<form action="" method="post">
+    <table class="all">
+        <tr>
+            <td class="tt">
+                <td class="pp">
+                    <input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>">
+                </td>
+            </td>
+        </tr>
+    </table>
+  <div class="ct">
+    <input type="submit" value="編輯">
+    <input type="reset" value="重置">
+  </div>
+</form>
