c6934ac2873b974278a0ff22616d42e2763eaaf9
Ethan <easonbest123@gmail.com>
2024-01-22 10:28:01 +0800

    加入網頁

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once "./api/db.php" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -33,7 +34,17 @@
             </span>
                     </div>
         <div id="right">
-        	        </div>
+        <?php
+             $do = $_GET['do'] ?? 'main';
+             $file = "./front/{$do}.php";
+             if (file_exists($file)) {
+             	include $file;
+             } else {
+             	include "./front/main.php";
+             }
+        ?>
+
+        </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
         	頁尾版權 :        </div>
     </div>
