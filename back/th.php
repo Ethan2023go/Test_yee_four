72cf692d9177173e4456bf913eeef3de07536a47
Ethan <easonbest123@gmail.com>
2024-01-31 14:22:16 +0800

    顯示商品分類類別功能  完成

--- back/th.php
+++ back/th.php
@@ -9,22 +9,34 @@
     <input type="text" name="mid" id="mid">
     <button onclick="addType('mid')">新增</button>
 </div>
-<!-- table.all>(tr.tt.ct>td+td>button*2)+(tr.pp.ct>td*2) -->
+<!-- table.all>(tr.tt>td+td.ct>button*2)+(tr.pp.ct>td*2) -->
 <table class="all">
-    <tr class="tt ct">
-        <td>流行皮件</td>
-        <td>
-            <button>修改</button>
-            <button>刪除</button>
+<?php
+$bigs=$Type->all(['big_id'=>0]);
+foreach($bigs as $big){
+?>    
+    <tr class="tt">
+        <td><?=$big['name'];?></td>
+        <td class="ct">
+            <button onclick="edit(this,<?=$big['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$big['id'];?>">刪除</button>
         </td>
     </tr>
+    <?php
+    $mids=$Type->all(['big_id'=>$big['id']]);
+    foreach($mids as $mid){
+    ?>
     <tr class="pp ct">
-        <td>女用皮件</td>
+        <td><?=$mid['name'];?></td>
         <td>
-            <button>修改</button>
-            <button>刪除</button>
+            <button onclick="edit(this,<?=$mid['id'];?>)">修改</button>
+            <button onclick="del('type',<?=$mid['id'];?>)">刪除</button>
         </td>
     </tr>
+    <?php
+      }
+    }
+  ?>
 </table>
 <script>
     getTypes(0)
