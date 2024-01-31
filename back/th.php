fc43901913b348205c2351c6d6bb6384dad44745
Ethan <easonbest123@gmail.com>
2024-01-31 14:00:59 +0800

    新增大中小分類  完成

--- back/th.php
+++ back/th.php
@@ -1,18 +1,19 @@
 <h2 class="ct">商品分類</h2>
 <div class="ct">
-    新增大分類 <input type="text" name="big" id="big"><button>新增</button>
+    新增大分類 <input type="text" name="big" id="big">
+    <button onclick="addType('big')">新增</button>
 </div>
 <div class="ct">
     新增中分類
     <select name="big" id="bigs"></select>
     <input type="text" name="mid" id="mid">
-    <button>新增</button>
+    <button onclick="addType('mid')">新增</button>
 </div>
-<!-- table.all>(tr.tt>td+td.ct>button*2)+(tr.pp.ct>td*2) -->
+<!-- table.all>(tr.tt.ct>td+td>button*2)+(tr.pp.ct>td*2) -->
 <table class="all">
-    <tr class="tt">
+    <tr class="tt ct">
         <td>流行皮件</td>
-        <td class="ct">
+        <td>
             <button>修改</button>
             <button>刪除</button>
         </td>
@@ -25,9 +26,36 @@
         </td>
     </tr>
 </table>
+<script>
+    getTypes(0)
+
+    function getTypes(big_id){
+        $.get("./api/get_types.php",{big_id},(types)=>{
+           $("#bigs").html(types)
+        })
+    } 
+    function addType(type) {
+        let name
+        let big_id;
+        switch(type){
+            case 'big':
+            name=$("#big").val();
+            big_id=0;
+            break;
+            case 'mid':
+               name=$("#mid").val();
+               big_id=$("#bigs").val();
+            break;
+        }
+        $.post("./api/save_type.php",{name,big_id},()=>{
+             location.reload();
+        })
+    }
+</script>
 
 <h2 class="ct">商品管理</h2>
 <!-- table.all>(tr.tt.ct>td*5)+(tr.pp>td*5) -->
+<div class="ct"><button>新增商品</button></div>
 <table class="all">
     <tr class="tt ct">
         <td>編號</td>
