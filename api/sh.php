a532be66584dcfc0c1a38f4eb376e6ed351fa13a
Ethan <easonbest123@gmail.com>
2024-02-16 08:59:41 +0800

    商品管理頁面 刪除 & 上下架功能 完成

---
+++ api/sh.php
@@ -0,0 +1,8 @@
+<?php
+include_once "db.php";
+
+$Goods->save($_POST);
+
+// 拿取 sh & id 
+// $_POST['sh']
+// $_POST['id']
--- back/th.php
+++ back/th.php
@@ -98,9 +98,9 @@ foreach($bigs as $big){
         <td><?=($good['sh']==1)?"上架":"下架";?></td>
         <td style="width:120px">
             <button>修改</button>
-            <button>刪除</button>
-            <button>上架</button>
-            <button>下架</button>
+            <button onclick="del('goods',<?=$good['id'];?>)">刪除</button>
+            <button onclick="sh(1,<?=$good['id'];?>)">上架</button>
+            <button onclick="sh(0,<?=$good['id'];?>)">下架</button>
         </td>
     </tr>
 
@@ -108,3 +108,11 @@ foreach($bigs as $big){
     }
     ?>
 </table>
+
+<script>
+    function sh(sh,id) {
+        $.post("./api/sh.php",{id,sh},()=>{
+            location.reload();
+        })
+      }
+</script>
