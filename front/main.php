1163a198b33b65adc9213553acba69c6e47dc416
Ethan <easonbest123@gmail.com>
2024-02-16 11:34:23 +0800

    首頁麵包屑功能  完成

--- front/main.php
+++ front/main.php
@@ -1,2 +1,19 @@
+<?php
+$type=$_GET['type']??0;
+$nav='';
 
-<button onclick="location.href='?do=buycart'">我要購買</button>
+if($type==0){
+    $nav="全部商品";
+}else{
+    $t=$Type->find($type);
+    if($t['big_id']==0){
+        $nav=$t['name'];
+    }else{
+        $b=$Type->find($t['big_id']);
+        $nav=$b['name']. " > " . $t['name'];
+    }
+}
+
+?>
+
+<h2><?=$nav;?></h2>
